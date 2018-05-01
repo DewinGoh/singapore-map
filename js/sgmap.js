26babf95ad3703d05b72365b67c33587ea324453
DewinGoh <dewin.goh@u.nus.edu>
2018-05-01 18:19:33 +0800

    fixed css and js elements

--- js/sgmap.js
+++ js/sgmap.js
@@ -31,59 +31,68 @@ function clearMap(m) {
 }
 
 var overlay; // can change this to a list in the future, to allow multiple overlays to be added at the same time
-
-$("#station_locations").click(function(){
-	clearMap(overlay);
-	$.ajax({
-	  type: 'GET',
-	  url: 'https://api.data.gov.sg/v1/environment/rainfall',
-	  cache: false,
-	  success: function(data){	
-		var locs = data.metadata["stations"]
-		var markerData = []
-		for (i=0;i<locs.length;i++) {
-			markerData.push(L.marker([locs[i]["location"]["latitude"],locs[i]["location"]["longitude"]]));
-		}
-		overlay = L.layerGroup(markerData).addTo(mymap);
-	}
-	});
-});
+var timeoutID;
+var legend;
+const rainfallColorMap = ['#FEEA06','#BEB83D','#7F8675','#3F54AD','#0022E5']
+const rainfallValues = [0,0.2,0.4,0.6,0.8]
 
 function colorMapWeather(value) {
-	if (value <= 0.1) {
-		return '#FEEA06'
-	} 
-	else if (value <= 0.3) {
-		return '#BEB83D'
+	for (j=1;j<rainfallValues.length;j++) {
+		if (value<=rainfallValues[j]){
+			return rainfallColorMap[j-1]
+		}
 	}
-	else if (value <= 0.5) {
-		return '#7F8675'
+	return rainfallColorMap[rainfallColorMap.length-1]
+}
+
+function clearGlobals() {
+	if (timeoutID == null){} else {
+		clearInterval(timeoutID);
 	}
-	else if (value <= 0.7) {
-		return '#3F54AD'
-	}
-	else {
-		return '#0022E5'
+
+	if (legend == null){} else {
+        mymap.removeControl(legend);
 	}
 }
 
-$("#rt_weather").click(function(){
-	clearMap(overlay);
+function getWeatherStations() {
 	$.ajax({
 	  type: 'GET',
 	  url: 'https://api.data.gov.sg/v1/environment/rainfall',
 	  cache: false,
 	  success: function(data){
+		clearMap(overlay);	
+		var locs = data.metadata["stations"]
+		var markerData = []
+		for (i=0;i<locs.length;i++) {
+			var lat = locs[i]["location"]["latitude"]
+			var lng = locs[i]["location"]["longitude"]
+			var popup = locs[i]["name"]
+			markerData.push(L.marker([lat,lng]).bindPopup(popup));
+		}
+		overlay = L.layerGroup(markerData).addTo(mymap);
+	}
+	});
+}
+
+function getRainfall() {
+	$.ajax({
+	  type: 'GET',
+	  url: 'https://api.data.gov.sg/v1/environment/rainfall',
+	  cache: false,
+	  success: function(data){
+		clearMap(overlay);
 		var locs = data.metadata["stations"]
 		var allweather = data.items[0].readings
 		var weatherData = []
+		var color;
 		for (i=0;i<locs.length;i++) {
 			stn = locs[i]["id"]
 			stn_name = locs[i]["name"]
 			stn_loc = [locs[i]["location"]["latitude"],locs[i]["location"]["longitude"]]
 			weather_stn = allweather[i]["station_id"]
 			weather_val = allweather[i]["value"]
-			var color = colorMapWeather(weather_val)
+			color = colorMapWeather(weather_val);
 			weatherData.push(
 			L.circle(stn_loc,{
 				color: color,
@@ -94,5 +103,52 @@ $("#rt_weather").click(function(){
 		}
 		overlay = L.layerGroup(weatherData).addTo(mymap);
 	}
+	
 	});
+}
+
+function createLegend(color_bins,bin_values,title){
+
+    legend = L.control({position: 'topright'});
+
+    legend.onAdd = function (mymap) {
+
+	    var div = L.DomUtil.create('div', 'info legend')
+
+	    div.innerHTML += '<p align="center"><b><u>' + title + '</u></b><br></p>'
+
+	    // loop through our density intervals and generate a label with a colored square for each interval
+	    for (var i=0;i<color_bins.length;i++) {
+	        if (i == color_bins.length-1){
+	            div.innerHTML +=
+	            '<i style="background:' + color_bins[i] + '"></i> ' +
+	            '>' + (bin_values[i]);
+
+	        } else {
+	                div.innerHTML +=
+	                '<i style="background:' + color_bins[i] + '"></i> ' + (bin_values[i]) + '&ndash;'+ bin_values[i+1] + '<br>';
+	            }
+	        }
+
+	        return div;
+	    };
+
+    legend.addTo(mymap);
+}
+
+
+
+
+$("#station_locations").click(function(){
+	clearGlobals();
+	getWeatherStations();
+	timeoutID = setInterval(getWeatherStations,60*1000);
+});
+
+
+$("#rt_weather").click(function(){
+	clearGlobals();
+	getRainfall();
+	createLegend(rainfallColorMap,rainfallValues,"Rainfall");
+	timeoutID = setInterval(getRainfall,60*1000);
 });
