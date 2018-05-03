9d6f9ebc8510d9420d3526887e7abbb9ab649ea2
DewinGoh <dewin.goh@u.nus.edu>
2018-05-03 10:34:15 +0800

    added real time carpark visualizations

--- js/sgmap.js
+++ js/sgmap.js
@@ -33,8 +33,10 @@ function clearMap(m) {
 var overlay; // can change this to a list in the future, to allow multiple overlays to be added at the same time
 var timeoutID;
 var legend;
-const rainfallColorMap = ['#FEEA06','#BEB83D','#7F8675','#3F54AD','#0022E5']
+const rainfallColorMap = ['#FEEA06','#BEB83D','#7F8675','#3F54AD','#0022E5'] // blue to grey to yellow
 const rainfallValues = [0,0.2,0.4,0.6,0.8]
+const availabilityColorMap = ['#ff0000','#ffa500','#ffff00','#9acd32','#00ff00'] // green to yellow to red
+const availabilityValues = [0,0.2,0.4,0.6,0.8]
 
 function colorMapWeather(value) {
 	for (j=1;j<rainfallValues.length;j++) {
@@ -43,6 +45,15 @@ function colorMapWeather(value) {
 		}
 	}
 	return rainfallColorMap[rainfallColorMap.length-1]
+}
+
+function colorCarparkAvailability(value) {
+	for (j=1;j<availabilityValues.length;j++) {
+		if (value<=availabilityValues[j]){
+			return availabilityColorMap[j-1]
+		}
+	}
+	return availabilityColorMap[availabilityColorMap.length-1]
 }
 
 function clearGlobals() {
@@ -136,8 +147,85 @@ function createLegend(color_bins,bin_values,title){
     legend.addTo(mymap);
 }
 
+function getCarparkLocations(){
+	var data = {
+	  resource_id: '139a3035-e624-4f56-b63f-89ae28d4ae4c' // the resource id
+	};
+	$.ajax({
+	  url: "https://data.gov.sg/api/action/datastore_search",
+	  // url: 'https://api.da.gov.sg/v1/transport/carpark-availability',
+  	  data: data,
+	  success: function(data){
+		clearMap(overlay);	
+		var carparks = data["result"]["records"]
+		var carparkData = []
+		var carparkPos = JSON.parse(carparkPosData);
+		var carparkNo,lat,lng,popup;
+		for (i=0;i<carparks.length;i++) {
+			carparkNo = carparks[i]["car_park_no"]
+			lat = carparkPos[carparkNo]["lat"]
+			lng = carparkPos[carparkNo]["lng"]
+			popup = '<b>'+carparkPos[carparkNo]["address"]+'</b><br><br>'+carparkPos[carparkNo]["car_park_type"]
+			carparkData.push(L.marker([lat,lng]).bindPopup(popup));
+		}
+		overlay = L.layerGroup(carparkData).addTo(mymap);
+	}
+	});
+}
+
+function getCarparkAvailability(){
+	$.ajax({
+	  type: 'GET',
+	  cache: true,
+	  url: 'https://api.data.gov.sg/v1/transport/carpark-availability',
+	  success: function(data){
+		clearMap(overlay);	
+		var carparks = data["items"][0]["carpark_data"]
+		var carparkData = []
+		var carparkPos = JSON.parse(carparkPosData);
+		var carparkNo,carparkInfo,popup,carparkCoords,lot_ratio;		
+		for (i=0;i<carparks.length;i++) {
+			// get coords
+			carparkNo = carparks[i]["carpark_number"]
+			try {
+				carparkCoords = [carparkPos[carparkNo]["lat"],carparkPos[carparkNo]["lng"]]
+			} 
+			catch(err){
+				continue
+			}
+			// get popup info
+			carparkInfo = carparks[i]["carpark_info"]
+			lots_available = carparkInfo[0]["lots_available"]
+			total_lots = carparkInfo[0]["total_lots"]
+			popup = '<b>'+carparkPos[carparkNo]["address"]+'</b><br><br>Lots Available: '+lots_available+'<br>Total Lots: '+total_lots
+
+			lot_ratio = lots_available/total_lots
+			color = colorCarparkAvailability(lot_ratio);
+			carparkData.push(
+			L.circle(carparkCoords,{
+				color: color,
+			    fillColor: color,
+			    fillOpacity: 0.7,
+			    radius: 100
+			}).bindPopup(popup));
+		}
+		overlay = L.layerGroup(carparkData).addTo(mymap);
+	}
+	});
+}
 
 
+$("#rt_carpark_availability").click(function(){
+	clearGlobals();
+	getCarparkAvailability();
+	createLegend(availabilityColorMap,availabilityValues,"Carpark Occupancy");
+	timeoutID = setInterval(getCarparkAvailability,60*1000);
+});
+
+$("#carpark_locations").click(function(){
+	clearGlobals();
+	getCarparkLocations();
+});
 
 $("#station_locations").click(function(){
 	clearGlobals();
@@ -149,7 +237,7 @@ $("#station_locations").click(function(){
 $("#rt_weather").click(function(){
 	clearGlobals();
 	getRainfall();
-	createLegend(rainfallColorMap,rainfallValues,"Rainfall");
+	createLegend(rainfallColorMap,rainfallValues,"Rainfall Level");
 	timeoutID = setInterval(getRainfall,60*1000);
 });
 
